8dac8e2f78b395fac0f848c26f8ac42526b457c6
tbwester <thomas_w@live.com>
2025-01-20 17:43:55 +0000

    change scaling strategy to none to avoid requesting extra blocks

--- sbnd_parsl/utils.py
+++ sbnd_parsl/utils.py
@@ -115,7 +115,7 @@ def create_default_useropts(**kwargs):
             'walltime': '1:00:00',
             'nodes_per_block' : 1,
             'cpus_per_node' : 32,
-            'strategy' : 'simple',
+            'strategy' : 'None',
         }
     else:
         # We're likely running locally
@@ -139,8 +139,8 @@ def create_parsl_config(user_opts, spack_opts=[]):
             executors=[executor],
             checkpoint_files=checkpoints,
             run_dir=user_opts["run_dir"],
-            strategy=user_opts.get("strategy", "simple"),
-            retries=user_opts.get("retries", 5),
+            strategy=user_opts.get("strategy", "None"),
+            retries=user_opts.get("retries", 0),
             app_cache=True,
     )
     '''
